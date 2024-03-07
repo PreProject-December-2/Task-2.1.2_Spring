1c0ebb933b47dd88a6995e829e24be05a78c683e
Anatoliy Basharov <rexonka3@gmail.com>
2024-03-07 16:37:38 +0300

    Finish task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean3 == bean4);
     }
 }
